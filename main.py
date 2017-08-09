fded68e755307ed88ffa6f0d4f7dfd35c2f2c775
tyki <dlxtt@msn.com>
2017-08-09 15:20:46 +0800

    first up

--- main.py
+++ main.py
@@ -18,8 +18,8 @@ class holeSocketServer(StreamRequestHandler):
         self.m_pp = protoProcesser(self)
         self.m_pp.dataProcess()
 
-cmd_ip_port = ('0.0.0.0', 1231)
-hole_ip_port = ('0.0.0.0', 6751)
+cmd_ip_port = ('0.0.0.0', 1232)
+hole_ip_port = ('0.0.0.0', 6752)
 
 #用户信息列表
 userlist = {}
@@ -39,11 +39,11 @@ class protoProcesser():
     def dataProcess(self):
         cmdgroup = {'PACKET_TYPE_INVALID': self.doUnknow,
                     'PACKET_TYPE_NEW_USER_LOGIN': self.doNewLogin,          # 服务器收到新的客户端登录，将登录信息发送给其他客户端
-                    'PACKET_TYPE_WELCOME': self.doLoginRet,                 # 客户端登录时服务器发送该欢迎信息给客户端，以告知客户端登录成功
+                    # 'PACKET_TYPE_WELCOME': self.doLoginRet,                 # 客户端登录时服务器发送该欢迎信息给客户端，以告知客户端登录成功
                     'PACKET_TYPE_REQUEST_CONN_CLIENT': self.doConnClient,   # 某客户端向服务器申请，要求与另一个客户端建立直接的TCP连接，即需要进行TCP打洞
-                    'PACKET_TYPE_REQUEST_MAKE_HOLE': self.doMakeHole,       # 服务器请求某客户端向另一客户端进行TCP打洞，即向另一客户端指定的外部IP和端口号进行connect尝试
+                    # 'PACKET_TYPE_REQUEST_MAKE_HOLE': self.doMakeHole,       # 服务器请求某客户端向另一客户端进行TCP打洞，即向另一客户端指定的外部IP和端口号进行connect尝试
                     'PACKET_TYPE_REQUEST_DISCONNECT': self.doDisconnect,    #请求服务器断开连接
-                    'PACKET_TYPE_TCP_DIRECT_CONNECT': self.doDirectConn,    # 服务器要求主动端（客户端A）直接连接被动端（客户端B）的外部IP和端口号
+                    # 'PACKET_TYPE_TCP_DIRECT_CONNECT': self.doDirectConn,    # 服务器要求主动端（客户端A）直接连接被动端（客户端B）的外部IP和端口号
                     'PACKET_TYPE_HOLE_LISTEN_READY': self.doListenReady,    # 被动端（客户端B）打洞和侦听均已准备就绪
                     'PACKET_TYPE_Logon': self.doLogon,
                     'PACKET_TYPE_UserList': self.doListenReady
@@ -60,7 +60,13 @@ class protoProcesser():
                 print("unknow command")
                 keepalive = False
 
+    #PORT2 被动端通知准备打洞 断开链接
     def doDisconnect(self):
+        #被动端补充信息 辅助流 辅助端口
+        destInfo = userlist[self.addr]
+        destInfo['holestream'] = self.m_srh
+        destInfo['holeport']=self.port
+
         return False
 
     def doUnknow(self):
@@ -70,40 +76,45 @@ class protoProcesser():
     def doNewLogin(self):
         try:
             # 记录socket和端口
-            userInfo = {'cmdstream':self.m_srh,'cmdport':self.port}
-            userlist[self.addr] = userInfo
+            nInfo = {'cmdstream':self.m_srh,'cmdport':self.port}
+            userlist[self.addr] = nInfo
 
-            self.m_srh.wfile.write(b'PACKET_TYPE_WELCOME')
+            # doLoginRet
+            #登录回应
+            self.m_srh.wfile.write(b'PACKET_TYPE_WELCOME\r\n')
         except:
             print("do New Login Error")
         return True
 
-    def doLoginRet(self):
-        pass
-    # PORT2 客户端发起主动连接请求
+    # PORT2 主动端发起主动连接请求
     def doConnClient(self):
         # 第二行为IP地址
         toaddr = str(self.m_srh.rfile.readline(), 'utf-8').strip("\r\n")
 
-        #先记录打洞信息
+        #主动端先记录打洞信息
+        #主动端补充信息  辅助流，辅助端口、目标ip
         userInfo = userlist[self.addr];
         userInfo['holestream']=self.m_srh
         userInfo['holeport']=self.port
         userInfo['toaddr'] = toaddr
 
-        #PORT1 发起打动请求
+        # doMakeHole
+        #被动端补充信息 目标IP 目标端口
         destInfo = userlist[toaddr]
         destInfo['toaddr'] = self.addr
         destInfo['toport'] = self.port
 
-        destInfo['cmdstream'].wfile.write(b'')
+        # PORT1 通知被动端 连接请求
+        destInfo['cmdstream'].wfile.write(b'PACKET_TYPE_REQUEST_MAKE_HOLE\r\n')
+        destInfo['cmdstream'].wfile.write(bytes(str(destInfo)+'\r\n','utf-8'))
         return True
 
-    def doMakeHole(self):
-        pass
+    # def doMakeHole(self):
+    #     pass
 
-    def doDirectConn(self):
-        pass
+    #PORT2
+    # def doDirectConn(self):
+    #     return False
 
     def doListenReady(self):
         pass
@@ -111,8 +122,18 @@ class protoProcesser():
     def doLogon(self):
         pass
 
+    #PORT1 被动端通知服务器，准备就绪
     def doListenReady(self):
-        pass
+        destInfo = userlist[self.addr]
+        #得到主动端信息
+        userInfo = userlist[destInfo['toaddr']]
+
+        #doDirectConn
+        #通知主动端发起访问 告知被动端端口
+        userInfo['cmdstream'].wfile.write(b'PACKET_TYPE_TCP_DIRECT_CONNECT\r\n')
+        userInfo['cmdstream'].wfile.write(bytes(str(destInfo)+'\r\n','utf-8'))
+
+        return True
 
 def tRun(tts):
     tts.serve_forever()
